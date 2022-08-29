7d590039f2997a01ec082b4196e256fe45c893d8
Niko <info@parlepeuple.fr>
2022-08-29 20:22:11 +0300

    Revert "Use mdb_page_loose() more"
    This reverts commit 12ee1a2d7104616ccb812db5d5f3548da507d9fc.

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -9989,7 +9989,7 @@ mdb_rebalance(MDB_cursor *mc)
 			mc->mc_db->md_root = P_INVALID;
 			mc->mc_db->md_depth = 0;
 			mc->mc_db->md_leaf_pages = 0;
-			rc = mdb_page_loose(mc, mp);
+			rc = mdb_midl_append(&mc->mc_txn->mt_free_pgs, mp->mp_pgno);
 			if (rc)
 				return rc;
 			/* Adjust cursors pointing to mp */
@@ -10017,7 +10017,7 @@ mdb_rebalance(MDB_cursor *mc)
 		} else if (IS_BRANCH(mp) && NUMKEYS(mp) == 1) {
 			int i;
 			DPUTS("collapsing root page!");
-			rc = mdb_page_loose(mc, mp);
+			rc = mdb_midl_append(&mc->mc_txn->mt_free_pgs, mp->mp_pgno);
 			if (rc)
 				return rc;
 			mc->mc_db->md_root = NODEPGNO(NODEPTR(mp, 0));
