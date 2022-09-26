4d66bccd20aad277044ed35dbae7e6b732a65b58
Niko <info@parlepeuple.fr>
2022-09-26 23:32:50 +0300

    adding some helper comments for enum indexes of MDB_cursor_op

--- libraries/liblmdb/lmdb.h
+++ libraries/liblmdb/lmdb.h
@@ -432,33 +432,33 @@ typedef void (MDB_sum_func)(const MDB_val *src, MDB_val *dst, const MDB_val *key
  *	using a cursor.
  */
 typedef enum MDB_cursor_op {
-	MDB_FIRST,				/**< Position at first key/data item */
-	MDB_FIRST_DUP,			/**< Position at first data item of current key.
+	MDB_FIRST,				/** 0 < Position at first key/data item */
+	MDB_FIRST_DUP,			/** 1 < Position at first data item of current key.
 								Only for #MDB_DUPSORT */
-	MDB_GET_BOTH,			/**< Position at key/data pair. Only for #MDB_DUPSORT */
-	MDB_GET_BOTH_RANGE,		/**< position at key, nearest data. Only for #MDB_DUPSORT */
-	MDB_GET_CURRENT,		/**< Return key/data at current cursor position */
-	MDB_GET_MULTIPLE,		/**< Return up to a page of duplicate data items
+	MDB_GET_BOTH,			/** 2 < Position at key/data pair. Only for #MDB_DUPSORT */
+	MDB_GET_BOTH_RANGE,		/** 3 < position at key, nearest data. Only for #MDB_DUPSORT */
+	MDB_GET_CURRENT,		/** 4 < Return key/data at current cursor position */
+	MDB_GET_MULTIPLE,		/** 5 < Return up to a page of duplicate data items
 								from current cursor position. Move cursor to prepare
 								for #MDB_NEXT_MULTIPLE. Only for #MDB_DUPFIXED */
-	MDB_LAST,				/**< Position at last key/data item */
-	MDB_LAST_DUP,			/**< Position at last data item of current key.
+	MDB_LAST,				/** 6 < Position at last key/data item */
+	MDB_LAST_DUP,			/** 7 < Position at last data item of current key.
 								Only for #MDB_DUPSORT */
-	MDB_NEXT,				/**< Position at next data item */
-	MDB_NEXT_DUP,			/**< Position at next data item of current key.
+	MDB_NEXT,				/** 8 < Position at next data item */
+	MDB_NEXT_DUP,			/** 9 < Position at next data item of current key.
 								Only for #MDB_DUPSORT */
-	MDB_NEXT_MULTIPLE,		/**< Return up to a page of duplicate data items
+	MDB_NEXT_MULTIPLE,		/** 10 < Return up to a page of duplicate data items
 								from next cursor position. Move cursor to prepare
 								for #MDB_NEXT_MULTIPLE. Only for #MDB_DUPFIXED */
-	MDB_NEXT_NODUP,			/**< Position at first data item of next key */
-	MDB_PREV,				/**< Position at previous data item */
-	MDB_PREV_DUP,			/**< Position at previous data item of current key.
+	MDB_NEXT_NODUP,			/** 11 < Position at first data item of next key */
+	MDB_PREV,				/** 12 < Position at previous data item */
+	MDB_PREV_DUP,			/** 13 < Position at previous data item of current key.
 								Only for #MDB_DUPSORT */
-	MDB_PREV_NODUP,			/**< Position at last data item of previous key */
-	MDB_SET,				/**< Position at specified key */
-	MDB_SET_KEY,			/**< Position at specified key, return key + data */
-	MDB_SET_RANGE,			/**< Position at first key greater than or equal to specified key. */
-	MDB_PREV_MULTIPLE		/**< Position at previous page and return up to
+	MDB_PREV_NODUP,			/** 14 < Position at last data item of previous key */
+	MDB_SET,				/** 15 < Position at specified key */
+	MDB_SET_KEY,			/** 16 < Position at specified key, return key + data */
+	MDB_SET_RANGE,			/** 17 < Position at first key greater than or equal to specified key. */
+	MDB_PREV_MULTIPLE		/** 18 < Position at previous page and return up to
 								a page of duplicate data items. Only for #MDB_DUPFIXED */
 } MDB_cursor_op;
 
